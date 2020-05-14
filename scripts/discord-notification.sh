f3c0a51d7a9a7ce3ae09f66c90c06c8207f903f6
Guillaume Bibaut <yom+gitlab@iaelu.net>
2020-05-14 15:25:34 +0000

    Update scripts/discord-notification.sh

--- scripts/discord-notification.sh
+++ scripts/discord-notification.sh
@@ -20,7 +20,6 @@ esac
 
 COMMITTER_NAME="$(git log -1 "$CI_COMMIT_SHA" --pretty="%cN")"
 COMMIT_SUBJECT="$(git log -1 "$CI_COMMIT_SHA" --pretty="%s")"
-COMMIT_MESSAGE="$(git log -1 "$CI_COMMIT_SHA" --pretty="%s")"
 
 
 TIMESTAMP=$(date --utc +%FT%TZ)
@@ -30,26 +29,23 @@ WEBHOOK_DATA='{
   "embeds": [ {
     "color": '$EMBED_COLOR',
     "author": {
-      "name": "'"$COMMITTER_NAME"'",
-      "url": "'"$CI_PIPELINE_URL"'",
-      "icon_url": "https://gitlab.com/favicon.png"
+      "name": "'$COMMITTER_NAME'",
     },
-    "title": "'"$COMMIT_SUBJECT"'",
-    "url": "'"$URL"'",
-    "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+    "title": "'$COMMIT_SUBJECT'",
+    "description": "'$COMMIT_SUBJECT'",
     "fields": [
       {
         "name": "Commit",
-        "value": "'"[\`$CI_COMMIT_SHORT_SHA\`]($CI_PROJECT_URL/commit/$CI_COMMIT_SHA)"'",
+        "value": "'"[$CI_COMMIT_SHORT_SHA](${CI_PROJECT_URL}/commit/${CI_COMMIT_SHA})"'",
         "inline": true
       },
       {
         "name": "Branch",
-        "value": "'"[\`$CI_COMMIT_REF_NAME\`]($CI_PROJECT_URL/tree/$CI_COMMIT_REF_NAME)"'",
+        "value": "'"[$CI_COMMIT_REF_NAME](${CI_PROJECT_URL}/tree/${CI_COMMIT_REF_NAME})"'",
         "inline": true
       }
     ],
-    "timestamp": "'"$TIMESTAMP"'"
+    "timestamp": "'$TIMESTAMP'"
   } ]
 }'
 
