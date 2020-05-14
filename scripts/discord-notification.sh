a8b1402f632b47665be2bfb4a8036d405d75876f
Guillaume Bibaut <yom@iaelu.net>
2020-05-14 19:02:17 +0200

    fix formating

--- scripts/discord-notification.sh
+++ scripts/discord-notification.sh
@@ -9,7 +9,7 @@ COMMIT_SUBJECT="$(git log -1 "$CI_COMMIT_SHA" --pretty="%s")"
 TIMESTAMP=$(date -u +%FT%TZ)
 WEBHOOK_DATA='{
   "username": "",
-  "content": "'$COMMIT_SUBJECT' commit: '"[$CI_COMMIT_SHORT_SHA](${CI_PROJECT_URL}/commit/${CI_COMMIT_SHA})"', branch: '"[$CI_COMMIT_REF_NAME](${CI_PROJECT_URL}/tree/${CI_COMMIT_REF_NAME})"'"
+  "content": "commit: "'"$COMMIT_SUBJECT"'", short sha: '"[$CI_COMMIT_SHORT_SHA](${CI_PROJECT_URL}/commit/${CI_COMMIT_SHA})"', branch: '"[$CI_COMMIT_REF_NAME](${CI_PROJECT_URL}/tree/${CI_COMMIT_REF_NAME})"'"
 }'
 
 curl --progress-bar -A "GitLabCI-Webhook" -H "Content-Type: application/json" -H "X-Author: y0m#7248" -d "$WEBHOOK_DATA" "$1"
