cbc2491589373aec136f474d19316a130ce28d9e
Guillaume Bibaut <yom@iaelu.net>
2020-05-14 18:53:33 +0200

    tested notification

--- scripts/discord-notification.sh
+++ scripts/discord-notification.sh
@@ -1,53 +1,15 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
-  echo "WARNING!! WEBHOOK_URL environment variable empty" && exit
+if [ -z "$1" ]; then
+  echo "WARNING!! WEBHOOK_URL environment variable empty" >&2 && exit
 fi
-
-echo "[Webhook]: Sending webhook to Discord...";
-
-case $1 in
-  "push" )
-    EMBED_COLOR=3066993
-    STATUS_MESSAGE="push"
-    ;;
-
-  * )
-    EMBED_COLOR=0
-    STATUS_MESSAGE="Status Unknown"
-    ;;
-esac
 
 COMMITTER_NAME="$(git log -1 "$CI_COMMIT_SHA" --pretty="%cN")"
 COMMIT_SUBJECT="$(git log -1 "$CI_COMMIT_SHA" --pretty="%s")"
-
-
-TIMESTAMP=$(date --utc +%FT%TZ)
+TIMESTAMP=$(date -u +%FT%TZ)
 WEBHOOK_DATA='{
   "username": "",
-  "avatar_url": "https://gitlab.com/favicon.png",
-  "embeds": [ {
-    "color": '$EMBED_COLOR',
-    "author": {
-      "name": "'$COMMITTER_NAME'",
-    },
-    "title": "'$COMMIT_SUBJECT'",
-    "description": "'$COMMIT_SUBJECT'",
-    "fields": [
-      {
-        "name": "Commit",
-        "value": "'"[$CI_COMMIT_SHORT_SHA](${CI_PROJECT_URL}/commit/${CI_COMMIT_SHA})"'",
-        "inline": true
-      },
-      {
-        "name": "Branch",
-        "value": "'"[$CI_COMMIT_REF_NAME](${CI_PROJECT_URL}/tree/${CI_COMMIT_REF_NAME})"'",
-        "inline": true
-      }
-    ],
-    "timestamp": "'$TIMESTAMP'"
-  } ]
+  "content": "'$COMMIT_SUBJECT' commit: '"[$CI_COMMIT_SHORT_SHA](${CI_PROJECT_URL}/commit/${CI_COMMIT_SHA})"', branch: '"[$CI_COMMIT_REF_NAME](${CI_PROJECT_URL}/tree/${CI_COMMIT_REF_NAME})"'"
 }'
 
-(curl --fail --progress-bar -A "GitLabCI-Webhook" -H "Content-Type:application/json" -H "X-Author:y0m#7248" -d "$WEBHOOK_DATA" "$2" \
-  && echo "[Webhook]: Successfully sent the webhook.") || echo "[Webhook]: Unable to send webhook."
+curl --progress-bar -A "GitLabCI-Webhook" -H "Content-Type: application/json" -H "X-Author: y0m#7248" -d "$WEBHOOK_DATA" "$1"
