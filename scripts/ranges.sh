a3bfd93470572e54f7cd5f147a5947696eb3aec2
Guillaume Bibaut <yom@iaelu.net>
2021-10-09 10:36:54 +0200

    base64 test

--- scripts/ranges.sh
+++ scripts/ranges.sh
@@ -33,13 +33,15 @@ for _rng in amazonaws googlecloud msazure spamhausdrop spamhausedrop; do
     if git diff --exit-code "${_rng}.json"; then
         echo "${_rng} no changes"
     else
-        _data="$(printf "{\"branch\": \"master\", \"author_email\": \"yom@iaelu.net\", \"author_name\": \"Guillaume Bibaut\", \"content\": \"%s\", \"commit_message\": \"%s changes\"}" "$(tr '\n' '@' < "${_rng}.json" | sed -e s/@/\\\\n/g -e s/\"/\\\\\"/g)" "${_rng}")"
+        _data="$(printf "{\"branch\": \"master\", \"author_email\": \"yom@iaelu.net\", \"author_name\": \"Guillaume Bibaut\", \"encoding\": \"base64\", \"commit_message\": \"%s changes\"}" "${_rng}")"
+        _base64="$(base64 -w0 "${_rng}.json")"
         _url="$(printf "https://gitlab.com/api/v4/projects/%s/repository/files/%s%%2Ejson" "${_PROJECT}" "${_rng}")"
 
         curl -sS --request PUT \
             --header "PRIVATE-TOKEN: ${_TOKEN}" \
             --header "Content-Type: application/json" \
             --data "${_data}" \
+            --data-urlencode "${_base64}" \
             "${_url}"
         _ret=$?
         if [ ${_ret} -ne 0 ]; then
