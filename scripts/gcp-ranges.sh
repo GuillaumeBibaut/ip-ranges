6365f144d17365959105e91b8bd887a3ffa2d960
Guillaume Bibaut <yom@iaelu.net>
2021-10-12 08:48:39 +0200

    use gstatic ip ranges url

--- scripts/gcp-ranges.sh
+++ scripts/gcp-ranges.sh
@@ -1,37 +1,18 @@
 #!/bin/sh
 
-LOOKUP="nslookup"
+_RANGES_URL="https://www.gstatic.com/ipranges/cloud.json"
+CURLCMD="curl"
 
-_netblocks="_cloud-netblocks.googleusercontent.com"
 _sep=""
-
-recursenb() {
-    local _nb _subnb _list
-
-    _nb=$1
-    if [ -z "${_nb}" ]; then
-        return 0
-    fi
-
-    _list=$(${LOOKUP} -q=TXT ${_nb} 8.8.8.8 | grep spf1 | sed -E -e 's:^.*text...::' -e 's:"::g')
-    for _item in ${_list}; do
-        case "${_item}" in
-        include*)
-            _include=$(printf "%s" "${_item}" | sed -E 's:include\:::')
-            recursenb "${_include}"
-            ;;
-        ip4*)
-            _ip4=$(printf "%s" "${_item}" | sed -E 's:ip4\:::')
-            printf "%s\n  \"%s\"" "${_sep}" "${_ip4}" >> googlecloud.json
-            _sep=","
-            ;;
-        esac
-    done
-}
-
 echo "[$(basename "$0")] Fetching Google Cloud Plateform IPv4 ranges..."
 printf "[" > googlecloud.json
-recursenb "${_netblocks}"
+while read -r _range; do
+    _rng=$(printf "%s" "${_range}" | sed -E 's:^.*ipv4Prefix"\: "(.*)".*$:\1:')
+    printf "%s\n  \"%s\"" "${_sep}" "${_rng}" >> googlecloud.json
+    _sep=","
+done <<DONE
+$(${CURLCMD} -sSL ${_RANGES_URL} | grep ipv4Prefix)
+DONE
 printf "\n]" >> googlecloud.json
 echo "[$(basename "$0")] Fetched Google Cloud Plateform IPv4 ranges."
 echo "[$(basename "$0")] Lines count: $(wc -l < googlecloud.json)"
