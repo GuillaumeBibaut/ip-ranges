ab1d42d234514f3ab1bc33671c646de3d6876f3c
Guillaume Bibaut <yom@iaelu.net>
2022-10-18 10:51:45 +0200

    2022 MS Azure IPv4 ranges

--- scripts/msa-ranges.sh
+++ scripts/msa-ranges.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-_FIRSTLONGPART="https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_"
-_INITIALURL="https://www.microsoft.com/EN-US/DOWNLOAD/confirmation.aspx?id=41653"
-_DATE=$(curl -sSL "${_INITIALURL}" | grep "PublicIPs_" | tail -n1 | sed -E "s:^.*018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_(.*)\.xml.*$:\1:")
+_INITIALURL="https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519"
+_DLURL=$(curl -sSL "${_INITIALURL}" | grep "ServiceTags_Public_" | tail -n1 | sed -E 's@^.*(https://download\.microsoft\.com/download/[^"]+/ServiceTags_Public_[^"]+.json)".*$@\1@')
 
 _sep=""
 echo "[$(basename "$0")] Fetching Microsoft Azure Cloud IPv4 ranges..."
 printf "[" > msazure.json
+
 while read -r _range; do
-    _rng=$(printf "%s" "${_range}" | sed -E 's:^.*Subnet="(.*)".*$:\1:')
-    if [ -z "${_rng}" ]; then
+    if [ -z "${_range}" ]; then
         break
     fi
-    printf "%s\n  \"%s\"" "${_sep}" "${_rng}" >> msazure.json
+    printf "%s\n  \"%s\"" "${_sep}" "${_range}" >> msazure.json
     _sep=","
 done <<DONE
-$(curl -sSL "${_FIRSTLONGPART}${_DATE}.xml" | grep IpRange)
+$(curl -sSL "${_DLURL}" | jq -r '.values[] | select(.properties.platform == "Azure") | .properties.addressPrefixes[]' | grep -E '^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}.*$')
 DONE
+
 printf "\n]" >> msazure.json
 echo "[$(basename "$0")] Fetched Microsoft Azure Cloud IPv4 ranges."
 echo "[$(basename "$0")] Lines count: $(wc -l < msazure.json)"
