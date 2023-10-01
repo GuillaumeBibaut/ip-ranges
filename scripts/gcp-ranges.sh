8b27761977d130fb3d0436e454eeb7afbac60b77
Guillaume Bibaut <yom@iaelu.net>
2023-10-01 08:13:32 +0000

    let jq do the job

--- scripts/gcp-ranges.sh
+++ scripts/gcp-ranges.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
+set -x
+set -e
+
 _RANGES_URL="https://www.gstatic.com/ipranges/cloud.json"
 CURLCMD="curl"
+JQCMD="jq"
 
-_sep=""
 echo "[$(basename "$0")] Fetching Google Cloud Plateform IPv4 ranges..."
-printf "[" > googlecloud.json
-while read -r _range; do
-    _rng=$(printf "%s" "${_range}" | sed -E 's:^.*ipv4Prefix"\: "(.*)".*$:\1:')
-    printf "%s\n  \"%s\"" "${_sep}" "${_rng}" >> googlecloud.json
-    _sep=","
-done <<DONE
-$(${CURLCMD} -sSL ${_RANGES_URL} | grep ipv4Prefix)
-DONE
-printf "\n]" >> googlecloud.json
+
+${CURLCMD} -sSL ${_RANGES_URL} | ${JQCMD}  '[.prefixes[] | select(.ipv4Prefix).ipv4Prefix]' >"googlecloud.json"
+
 echo "[$(basename "$0")] Fetched Google Cloud Plateform IPv4 ranges."
 echo "[$(basename "$0")] Lines count: $(wc -l < googlecloud.json)"
 
